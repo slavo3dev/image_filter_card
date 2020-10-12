6dca1c2c23d45b4379f95e3e4aa0f265f0fb622b
Slavoljub Popovic <18499909+slavo3dev@users.noreply.github.com>
2020-10-12 14:01:58 -0400

    adding simple container

--- src/InfoContainer.js
+++ src/InfoContainer.js
@@ -4,16 +4,19 @@ import "./InfoContainer.css";
 export function InfoContainer() {
   return (
     <div className="infoContainer">
-      <div className="infoIcon">
-        <div>
+      <div className="infoTextIconContainer">
+        <div className="IconContainer">
           <p>ICON</p>
         </div>
-        <div>
+        <div className="infoTextContainer">
           <p>Please Say it</p>
         </div>
       </div>
-      <div className="closeBtnContainer">
-        <p>x</p>
+      <div className="textInfoContainer">
+        <p>
+          “Please carefully hold your test card up to your camera and take a
+          photo.”
+        </p>
       </div>
     </div>
   );
