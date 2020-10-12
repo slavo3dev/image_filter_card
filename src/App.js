97e8831f75e76737457c0a7234831ee84ccff0e8
Slavoljub Popovic <18499909+slavo3dev@users.noreply.github.com>
2020-10-12 15:11:07 -0400

    adding image container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles.css";
 import { CloseTopContainer } from "./closeTopContainter";
 import { InfoContainer } from "./InfoContainer";
+import { ImageContainter } from "./ImageContainer";
 export default function App() {
   return (
     <div className="imageContainer">
       <CloseTopContainer />
       <InfoContainer />
+      <ImageContainter />
     </div>
   );
 }
---
+++ src/ImageContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ImageContainter.css";
+
+export function ImageContainter() {
+  return (
+    <div className="ImgContainter">
+      <p>Photo will appear here</p>
+    </div>
+  );
+}
